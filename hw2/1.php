5356d6af9030bdd88e76cc86e04b87e9ad29be9b
Timur G <timur91g@gmail.com>
2021-11-18 18:08:11 +0300

    test

--- hw2/1.php
+++ hw2/1.php
@@ -2,3 +2,18 @@
 /**
  * Заглушка
  */
+
+class BaseClass {
+     function __construct() {
+         echo "Конструктор класса BaseClass\n";
+     }
+}
+class SubClass extends BaseClass {
+     function __construct() {
+         parent::__construct();
+         echo "Конструктор класса SubClass\n";
+     }
+}
+$obj = new BaseClass();
+$obj = new SubClass();
+?>
