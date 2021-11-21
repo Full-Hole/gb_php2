285f119e47486fde1970c106e5edb5e498cc938d
Timur G <timur91g@gmail.com>
2021-11-21 21:59:21 +0300

    create gallery

---
+++ hw3/public/js/main.js
@@ -0,0 +1,31 @@
+
+"use strict"
+//let x = document.getElementsByClassName('gallery-item');
+let viewport = document.querySelector('.viewport');
+//let lWidth = window.screen.width
+// document.addEventListener('.gallery-item');
+ let imgList = document.querySelectorAll(".gallery-img");
+
+viewport.src=imgList[0].src;
+ imgList.forEach(img => img.addEventListener('click', switchImg))
+//console.log(x);
+
+function roomba(event){
+    console.log(event);
+}
+function switchImg(event){
+    viewport.src=event.target.src;
+}
+
+viewport.addEventListener('click', fullview);
+
+function fullview(event){
+    let gallery_viewport = event.path[2];
+    let viewport_wraper = event.path[1];
+    gallery_viewport.classList.toggle("gallery-viewport-fullscreen");
+    viewport_wraper.classList.toggle("vieport-wrapper");
+    viewport_wraper.classList.toggle("vieport-wrapper-fullscreen");
+
+    //console.log(gallery_viewport);
+}
+
