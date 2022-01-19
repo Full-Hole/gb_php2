ead81d47ea42b78bf0a7f3994af5ec15e8b52d74
Timur G <timur91g@gmail.com>
2022-01-19 15:20:18 +0300

    Add child class

--- hw1/1-4.php
+++ hw1/1-4.php
@@ -12,12 +12,18 @@ class Product{
         $this->price = $price;
         $this->description = $description;
         $this->img = $img;
+    }
+    public function getName(){
+            return $this->name;
         }
-
-    function getPrice(){
+        
+    public function getPrice(){
         return $this->price;
     }
-    function render(){
+    public function getDesctription(){
+        return $this->desctription;
+    }
+    protected function render(){
         return "<div><h3>$this->name</h3><img src=\"$this->img\"><p>$this->description</p><h4>$this->price</h4></div>";
     }        
 
@@ -26,14 +32,35 @@ class Product{
 class ProductInfo extends Product{
     protected $imgList;
     protected $specs;
-
+    
+   
     private function loadProduct(){
      /*Load data from DB */   
     }
 
 }
 
+class PhoneProduct extends Product{
+    private $simCardCount;
+    protected $specs;
+
+    public function __construct($id, $name, $price, $description = "No description", $img,$simCardCount) 
+    {
+        parent::__construct($id, $name, $price, $description = "No description", $img);
+        $this->simCardCount = $simCardCount;
+    }
+    public function getSimCardCount(){
+        return $this->simCardCount;
+    }
+    public function render(){
+        return parent::render()."<div>Количесвто SIM-карт: $this->simCardCount</div>";
+    }
+    
+
+
+}
+
 // $prod = new Product(1,"Table", '2200.00', 'No descr',"nop");
 // echo $prod -> getPrice() . PHP_EOL;
 // echo $prod -> render() . PHP_EOL;
-//$single = new ProductInfo()
+//$phone = new PhoneProduct(1,"Table", '2200.00', 'No descr',"nop", 2);
