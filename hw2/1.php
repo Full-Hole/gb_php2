6da89ad92974ccf20e96d8163955b5a6a1c60e9b
Timur G <timur91g@gmail.com>
2022-01-21 16:33:53 +0300

    update 1.php

--- hw2/1.php
+++ hw2/1.php
@@ -1,8 +1,7 @@
 <?php
 
 /*
- * Заглушка
- * 1. Создать структуру классов ведения товарной номенклатуры.
+ 1. Создать структуру классов ведения товарной номенклатуры.
 а) Есть абстрактный товар.
 б) Есть цифровой товар, штучный физический товар и товар на вес.
 в) У каждого есть метод подсчета финальной стоимости.
@@ -17,51 +16,94 @@ namespace hw2;
 
 abstract class Product
 {
-    private $quantity;
-    protected static $price = 1050;
+    const PROFIT_PERCENT = 10;
+    //private $quantity; 
     
-    abstract protected function calcPrice();
-    public function getPrice()
-    {
-        return $this->calcPrice();
+    abstract public function calcPrice(); //подсчет общего итоговой суммы
+    //abstract protected function calcProfit(); //подсчет дохода
+    abstract public function getPrice(); // получить стоимость единицы товара
+
+    public function calcProfit(){
+        return $this->calcPrice()/100 * self::PROFIT_PERCENT;
     }
+    // public function getPrice()
+    // {
+    //     return $this->calcPrice();
+    // }
 }
 
 class digitalProduct extends Product
 {
-    // function __construct(int $quantity)
-    // {
-    //     $this->quantity = $quantity;
-    // }
-    protected function calcPrice()
-    {
-        return (parent::$price / 2)."$ per unit".PHP_EOL;
-    }
-}
-class simpleProduct extends Product
-{
-
-    protected function calcPrice()
-    {
-        return parent::$price."$ per unit".PHP_EOL;
-    }
-}
-class weightProduct extends Product
-{
-    function __construct(float $quantity)
+    private $quantity;
+    const PRICE = 1050;
+    function __construct(int $quantity)
     {
         $this->quantity = $quantity;
     }
-    protected function calcPrice()
+
+    public function getPrice(){
+        return self::PRICE;
+    }
+    public function calcPrice()
     {
-        return $this->quantity * parent::$price."$ per $this->quantity".PHP_EOL;
+        return $this->getPrice() * $this->quantity;
+        
+    }
+
+}
+class simpleProduct extends digitalProduct
+{
+
+    function __construct(int $quantity)
+    {
+        $this->quantity = $quantity;
+        $this->price = parent::PRICE;
+    }
+    public function getPrice(){
+        
+        return parent::PRICE * 2;
+    }
+    public function calcPrice()
+    {
+        return $this->getPrice() * $this->quantity;
+        
+    }
+    
+}
+class weightProduct extends Product
+{
+    private $price;
+    function __construct($price, float $quantity)
+    {
+        $this->price = $price;
+        $this->quantity = $quantity;
+    }
+    public function getPrice(){
+        return $this->price;
+    }
+    public function calcPrice()
+    {
+        return $this->quantity * $this->price;
     }
 }
 
 
-$digital = new digitalProduct();
-$simple = new simpleProduct();
-$weight = new weightProduct(0.25);
-echo $digital->getPrice();
-echo $simple->getPrice();
-echo $weight->getPrice();
+$digital = new digitalProduct(2);
+$simple = new simpleProduct(2);
+$weight = new weightProduct(100, 0.25);
+// echo $digital->getPrice();
+// echo $simple->getPrice();
+// echo $weight->getPrice();
+
+echo $digital->getPrice() . ' За единицу товара <br/>';
+echo $digital->calcPrice() . ' Всего <br/>';
+echo $digital->calcProfit() . ' Доход <br/>';
+
+echo $simple ->getPrice() . ' За единицу товара <br/>';
+echo $simple ->calcPrice() . ' Всего <br/>';
+echo $simple ->calcProfit() . ' Доход <br/>';
+
+echo $weight ->getPrice() . ' За единицу 100гр <br/>';
+echo $weight ->calcPrice() . ' Всего <br/>';
+echo $weight ->calcProfit() . ' Доход <br/>';
+
