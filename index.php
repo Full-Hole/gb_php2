91e5a9a6e547cf572d3e013daa52d3efc0a51989
Timur G <timur91g@gmail.com>
2022-02-02 20:52:34 +0300

    prepare

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<a href='hw3/1/public/index.php'> Index 1</a><br/>";
+echo "<a href='hw3/2/public/index.php'> Index 2</a><br/>";
+echo "<a href='work/sess/page1.php'>Page 1</a><br/>";
+echo "<a href='work/sess/page2.php'>Page 2</a><br/>";
+echo "<a href='hw5/index.php'> Index 5</a><br/>";
+echo "<a href='work/l2/spl_autoload.php'>spl autoload </a><br/>";
+
